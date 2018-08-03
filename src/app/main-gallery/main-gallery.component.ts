c7d1f8b2048321cf4df1cce4f4ff22e10b6c9db8
mikesmobile <webdesigner.mikesmobile@gmail.com>
2018-08-03 14:31:14 -0700

    main gallery "works" but needs heavy styling. Some light content edits for mike.

--- src/app/main-gallery/main-gallery.component.ts
+++ src/app/main-gallery/main-gallery.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-main-gallery',
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainGalleryComponent implements OnInit {
 
+  @Input() Images
+  @ViewChild('content') public contentModal;
+  public name: string;
+
+  public source:string;
   constructor() { }
 
   ngOnInit() {
+    console.log(this.Images);
   }
+  
 
+    show(value:string){
+        this.name = value;
+        this.contentModal.show();
+    }
 }
