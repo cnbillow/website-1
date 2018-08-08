7d202115aabbd9a47f3b049e7740c05ca89686d5
mikesmobile <webdesigner.mikesmobile@gmail.com>
2018-08-08 12:12:46 -0700

    next and previous buttons for main gallery

--- src/app/main-gallery/main-gallery.component.ts
+++ src/app/main-gallery/main-gallery.component.ts
@@ -1,33 +1,62 @@
-import { Component, OnInit, Input, ViewChild } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, OnChanges, SimpleChanges } from '@angular/core';
 import { ViewEncapsulation } from '@angular/core'
-
 @Component({
   encapsulation: ViewEncapsulation.None,
   selector: 'app-main-gallery',
   templateUrl: './main-gallery.component.html',
   styleUrls: ['./main-gallery.component.sass']
 })
-export class MainGalleryComponent implements OnInit {
+export class MainGalleryComponent implements OnInit,OnChanges {
 
   @Input() Images
-  @ViewChild('mainContent') public contentModal;
+  @ViewChild('mainContent') public contentModal:any;
+  @ViewChild('mainCarousel') public contentCarousel; 
+  @ViewChild('modalImage') public modalImage;
   public name: string;
-  public index=0;
+  public reload: boolean = false;
+  public index: number = 0;
   public source:string;
+  public imagesSize;
   constructor() { }
 
   ngOnInit() {
-    console.log(this.Images);
+    this.imagesSize= this.Images.length
+  }
+  ngOnChanges(changes: SimpleChanges){
+    console.log(changes)
   }
   
+  activeSlideChange(event: any){
+    console.log(event);
+}
 
-    show(value:string){
+  hideCheck(event){
+    console.log(event)
+
+    this.contentCarousel.selectSlide(0);
+    if(this.reload){
+    this.contentModal.show();
+    this.reload=false;
+  }
+  }
+  show(){
         
-        this.name = value
+        this.name = this.Images[this.contentCarousel._currentActiveSlide].big
         this.contentModal.show();
     }
-    increment(event){
-      //event.preventdefault();
- 
+  next(){
+      console.log(this.contentCarousel._currentActiveSlide);
+      
+
+      this.contentCarousel.nextSlide();
+      console.log(this.modalImage)
+       this.modalImage.nativeElement.setAttribute('src',this.Images[(this.contentCarousel._currentActiveSlide+1)%this.imagesSize].big)
+    }
+    previous(){
+      let nextIndex = this.contentCarousel._currentActiveSlide-1;
+      if(this.contentCarousel._currentActiveSlide===0)
+        nextIndex=this.imagesSize-1
+      this.contentCarousel.previousSlide();
+      this.modalImage.nativeElement.setAttribute('src',this.Images[nextIndex].big)
     }
 }
