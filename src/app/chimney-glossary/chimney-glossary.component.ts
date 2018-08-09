42e9df76ef9b08732615eba1ef235e338163a68e
mikesmobile <webdesigner.mikesmobile@gmail.com>
2018-08-09 14:35:18 -0700

    added chimney glossary component and json for configuration

---
+++ src/app/chimney-glossary/chimney-glossary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chimney-glossary',
+  templateUrl: './chimney-glossary.component.html',
+  styleUrls: ['./chimney-glossary.component.sass']
+})
+export class ChimneyGlossaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
